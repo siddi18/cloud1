71076f9a2f9632674bdf2942f99d34fd5dd33c17
Siddhi Vinayak <144713105+siddi18@users.noreply.github.com>
2025-02-08 11:53:36 +0530

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,6 +1,6 @@
 window._config = {
     cognito: {
-        userPoolId: 'je0rdc', // e.g. us-east-2_uXboG5pAb
+        userPoolId: 'ap-south-1_GWjz9ymCI', // e.g. us-east-2_uXboG5pAb
         userPoolClientId: '7qnmrfmtoud5j1nnajf34bsv0k', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'ap-south-1' // e.g. us-east-2
     },
