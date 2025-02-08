e768f2e9200d17368f4b70940f9f9fa30ecfd595
Siddhi Vinayak <144713105+siddi18@users.noreply.github.com>
2025-02-08 16:13:26 +0530

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,11 +1,11 @@
 window._config = {
     cognito: {
-        userPoolId: 'ap-south-1_GWjz9ymCI', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '4j06j75a6ll4djuubei59b8e5q', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        client_secret: '63ahrebhpcjj7kcaua3167covmmffffcu4i1u98tol67uenjh1u',
+        userPoolId: 'ap-south-1_nuPKHJjpa', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '240burhsimmfp2f13dhmhf9g5o', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        client_secret: '1pdlshsghkme61fn2bo1f1v66bmntdoj3cj3a9b0vanfjrd3ct8r',
         region: 'ap-south-1' // e.g. us-east-2
     },
     api: {
-        invokeUrl: 'https://miq87fm8ef.execute-api.eu-west-1.amazonaws.com/prod' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+        invokeUrl: 'https://master.d2m6b5lsx6z2cy.amplifyapp.com/prod' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     }
 };
